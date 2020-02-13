5752d1ba6d1d3aeb8da359ebc29e48b2a98d082c
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-13 23:26:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer)
-  planeteer.map {|one| one.upcase + "!"}
+  planeteer.map {|one| one.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
